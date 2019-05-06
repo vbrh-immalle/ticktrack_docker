754732241328de387c48d979c81008fe97739deb
Hans Van Broeckhoven <hans.vanbroeckhoven@immalle.be>
2019-05-06 11:23:39 +0200

    Voeg commentaar toe i.v.m. Docker-specifieke dingen

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    hostname = socket.gethostname()
+    hostname = socket.gethostname() # We willen dat de webapplicatie de hostname van de server toont omdat meerdere Docker-instanties verschillende hostnames hebben
     return f"<p>Hello! (Being served from {hostname})</p>"
 
 
+# Om onderscheid tussen docker en lokale ontwikkelomgeving (die op poort 8080 draait) te maken
 port = 80
 if socket.gethostname() == "xubimma":
     port = 8080
